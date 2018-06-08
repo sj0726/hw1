0eaf72bbc1183213de949c93eda01c1d2e8103d2
sj0726 <123a556@naver.com>
2018-06-08 16:09:06 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,43 +3,43 @@ const router = express.Router();
 const request = require('request-promise')
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/hw2')
+mongoose.connect('mongodb://localhost/HW2')
 const db = mongoose.connection
 db.once('open', function () {
     console.log('Connection successful.')
 })
 const Schema = mongoose.Schema
-const stringSchema = new Schema({
+const stringa = new Schema({
     string: String,
     length: Number
 })
-const stringModel = mongoose.model('Strings', stringSchema)
+const Model = mongoose.model('Person', stringa)
 
 router.get('/', function(req, res, next){
-    let arr = Person.find({}, '-_id -__v', function(err, result){
+    let arr = Model.find({}, '-_id -__v', function(err, result){
         res.json(result)
     })
 })
 
 /* GET home page. */
 router.get('/:str', function(req, res, next) {
-    let input = req.params.str
-    const inputString = new stringModel({string: input}, function (err, result){
+    let mine = req.params.str
+    const jsonthing = Model.find({string: mine}, function (err, result) {
         if (result.length == 0){
-            let mymodel = new stringModel({string: input, length: input.length})
+            let mymodel = new Model({string: mine, length: mine.length})
             mymodel.save(function(err){
                 if (err) console.log('Error saving')
 
                 console.log('Saved')
-                res.json({string: input, length: input.length})
+                res.json({string: mine, length: mine.length})
             })
         }
-        else{
+        else {
             console.log('Exists in DB')
-            res.json({string: input, length: input.length})
+            res.json({string: mine, length: mine.length})
         }
-    })
-});
+    });
+})
 
 
 router.post('/', function(req, res, next){
@@ -51,9 +51,9 @@ router.post('/', function(req, res, next){
         res.json(errMsg)
     }
     else{
-        const inputString = stringModel.find({string: input}, function (err, result){
+        const inputString = Model.find({string: input}, function (err, result){
             if (result.length == 0) {
-                let mymodel = new stringModel({string: input, length: input.length})
+                let mymodel = new Model({string: input, length: input.length})
                 mymodel.save(function (err){
                     if (err) console.log('Error Saving')
 
@@ -69,9 +69,9 @@ router.post('/', function(req, res, next){
     }
 })
 
-router.delete('/:str', function(req, res, next){
+router.delete('/', function(req, res, next){
     const input = req.body.string
-    const inputString = stringModel.find({string: input}, function (err, result){
+    const inputString = Model.find({string: input}, function (err, result){
         if (result.length == 0){
             let errMsg = {
                 message: 'String not found in DB'
@@ -79,7 +79,7 @@ router.delete('/:str', function(req, res, next){
             res.json(errMsg)
         }
         else{
-            stringModel.deleteOne({string: input}, function (err){
+            Model.deleteOne({string: input}, function (err){
                 if (err){
                     console.log('Error Deleting')
                 }
@@ -95,3 +95,4 @@ router.delete('/:str', function(req, res, next){
 })
 
 module.exports = router;
+
